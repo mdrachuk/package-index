f1769a1f10615d2e84cdd99a65a62fe7668d8771
mdrachuk <misha@drach.uk>
2019-09-18 17:57:21 +0300

    Add TODO to test

--- tests.py
+++ tests.py
@@ -6,6 +6,7 @@ from package_index import fetch_package, PackageIndexError
 
 
 def test_fetch_self():
+    # TODO:mdrachuk:9/18/19: change to package-index
     package = fetch_package('serious')
     assert package.contains_version('1.0.0.dev20')
 
